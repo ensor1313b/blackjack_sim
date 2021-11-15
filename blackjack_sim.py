72201c356388ed2766e71f0b877a611d8c520fce
ensor1313b <mmathog@gmail.com>
2021-11-15 10:16:19 -0800

    adding first blackjack sim code

---
+++ blackjack_sim.py
@@ -0,0 +1,11 @@
+
+#blackjack simulator
+
+##card array (tuple?, just values, need to deal with aces)
+
+##auto-rules (e.g. 'when dealer shows X, player does Y')
+
+##money betting mgmt scheme (e.g. '2123345')
+
+
+### main question... at low stakes table, is there a sufficiently small stake s.t. you can "reliably" (high probability) win $200 (walk away) and 'earn a living' playing (non-cheating) blackjack?
