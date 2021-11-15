4768fa2b600947af70519a2de335e19ceadc2a84
ensor1313b <mmathog@gmail.com>
2021-11-15 11:10:35 -0800

    adding next comment  blackjack sim code

--- blackjack_sim.py
+++ blackjack_sim.py
@@ -8,4 +8,7 @@
 ##money betting mgmt scheme (e.g. '2123345')
 
 
-### main question... at low stakes table, is there a sufficiently small stake s.t. you can "reliably" (high probability) win $200 (walk away) and 'earn a living' playing (non-cheating) blackjack?
+### main question... at low stakes table, is there a sufficiently small stake s.t. you can "reliably" (high probability) win $200 (walk away) and 'earn a living' playing (non-cheating) blackjack
+
+
+### now i make a change
